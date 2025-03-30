daa87e545f10b97d7ea8888732c503e5a2dc5846
Nadil <liyanage.nadil@gmail.com>
2025-03-30 17:47:09 +0530

    notification added
    notification added

--- backend/notification-service/models/Notification.js
+++ backend/notification-service/models/Notification.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const notificationSchema = new mongoose.Schema(
   {
     userId: { type: mongoose.Schema.Types.ObjectId, required: true },
-    type: { type: String, enum: ["email", "sms"], required: true },
+    type: { type: String, enum: ["email", "sms", "whatsapp"], required: true },
     message: { type: String, required: true },
     status: {
       type: String,
