31cb0d06d9759e3e7f8d5d011072d17b04812fda
Peiris M. H. C. <154981814+IT22194930@users.noreply.github.com>
2025-04-29 10:39:19 +0530

    removed delivery personnel dashboard items

--- frontend/src/pages/Home/DeliveryPersonnel/DeliveryMap.jsx
+++ frontend/src/pages/Home/DeliveryPersonnel/DeliveryMap.jsx
@@ -3,10 +3,10 @@ import React from 'react';
 const DeliveryMap = () => {
   return (
     <div className="delivery-map-container">
-      <h2>Delivery Map</h2>
+      {/* <h2>Delivery Map</h2> */}
       <div className="map-container">
         {/* Map implementation will go here */}
-        <p>Map showing current delivery locations and routes will be displayed here</p>
+        {/* <p>Map showing current delivery locations and routes will be displayed here</p> */}
       </div>
     </div>
   );
--- frontend/src/pages/Home/DeliveryPersonnel/RecentDeliveries.jsx
+++ frontend/src/pages/Home/DeliveryPersonnel/RecentDeliveries.jsx
@@ -3,10 +3,10 @@ import React from 'react';
 const RecentDeliveries = () => {
   return (
     <div className="recent-deliveries-container">
-      <h2>Recent Deliveries</h2>
+      {/* <h2>Recent Deliveries</h2> */}
       <div className="deliveries-list">
         {/* List of recent deliveries will go here */}
-        <p>List of recent deliveries and their status will be displayed here</p>
+        {/* <p>List of recent deliveries and their status will be displayed here</p> */}
       </div>
     </div>
   );
